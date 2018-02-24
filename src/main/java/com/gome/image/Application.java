9e18038cc076b7a06ac7138e1fc299119d627acc
PasseRR <xie__hai@sina.com>
2018-02-24 10:19:47 +0800

    initial commit

---
+++ src/main/java/com/gome/image/Application.java
@@ -0,0 +1,17 @@
+package com.gome.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用入口
+ * @author xiehai1
+ * @date 2018/02/24 10:14
+ * @Copyright(c) gome inc Gome Co.,LTD
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
